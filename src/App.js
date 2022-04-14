9ac2c421d358cd5541e68b09010907baf3c6c04c
Rafiqul Islam Tusher <md.rafiqul26997@gmail.com>
2022-04-14 16:39:37 +0600

    first commit to project firebase & router  init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import NotFound from './pages/404/NotFound';
+import About from './pages/about/About';
+import Contact from './pages/contact/Contact';
+import Home from './pages/home/Home';
+import Footer from './pages/shared/footer/Footer';
+import Header from './pages/shared/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/pages/shared/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hader = () => {
+    return (
+        <div>
+            <h1>Header</h1>
+        </div>
+    );
+};
+
+export default Hader;
